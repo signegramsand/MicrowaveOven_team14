0d191e7c87716ebefa72b02ac0c05198f2af1034
LarsMoelgaard <60734425+LarsMoelgaard@users.noreply.github.com>
2021-04-21 09:30:38 +0200

    Oprettelse af testprojekt

---
+++ Microwave_InteractionTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Microwave_InteractionTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
