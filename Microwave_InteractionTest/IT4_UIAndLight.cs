ba5b28a26b48db56fcec8342b5c08bb80d5bf68f
LarsMoelgaard <60734425+LarsMoelgaard@users.noreply.github.com>
2021-04-28 09:31:25 +0200

    Færdiggørelse af IT step 4 og oprettelse af IT5

---
+++ Microwave_InteractionTest/IT4_UIAndLight.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave_InteractionTest.Test.Unit
+{
+    [TestFixture]
+    public class IT4_UIAndLight
+    {
+        private Door doorSut;
+        private UserInterface UI;
+
+        private Button powerButtonSut;
+        private Button timeButtonSut;
+        private Button startCancelButtonSut;
+
+        private Light light;
+
+        private CookController cooker;
+
+        private IDisplay display;
+        
+        private IOutput output;
+
+        private ITimer timer;
+        private IPowerTube powerTube;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+
+
+            timer = Substitute.For<ITimer>();
+            powerTube = Substitute.For<IPowerTube>();
+            display = Substitute.For<IDisplay>();
+            output = Substitute.For<IOutput>();
+
+
+
+            light = new Light(output);
+            cooker = new CookController(timer, display, powerTube);
+
+
+
+            doorSut = new Door();
+            powerButtonSut = new Button();
+            timeButtonSut = new Button();
+            startCancelButtonSut = new Button();
+
+            UI = new UserInterface(powerButtonSut, timeButtonSut, startCancelButtonSut, doorSut, display, light, cooker);
+
+
+            cooker.UI = UI;
+
+            //State is ready by default
+
+        }
+
+        [Test]
+        public void StateReady_DoorOpen_LightTurnsOnAndOutputIsCalled()
+        {
+            //Act
+            doorSut.Open();
+
+            //Assert
+            output.Received(1).OutputLine(Arg.Is<string>(str => str.ToLower().Contains("on")));
+        }
+
+
+
+        [Test]
+        public void StateDoorOpen_DoorClose_LightTurnsOffAndOutputIsCalled()
+        {
+            //Arrange
+            doorSut.Open();
+            output.ClearReceivedCalls();
+
+            //Act
+            doorSut.Close();
+
+
+            //Assert
+            output.Received(1).OutputLine(Arg.Is<string>(str => str.ToLower().Contains("off")));
+            
+        }
+    }
+}
---
+++ Microwave_InteractionTest/IT5_CookControlAndTimer.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using Timer = Microwave.Classes.Boundary.Timer;
+
+namespace Microwave_InteractionTest.Test.Unit
+{
+    [TestFixture]
+    public class IT5_CookControlAndTimer
+    {
+        private Door doorSut;
+        private UserInterface UI;
+
+        private Button powerButtonSut;
+        private Button timeButtonSut;
+        private Button startCancelButtonSut;
+
+        private Light light;
+        private Timer timer;
+
+
+        private CookController cooker;
+
+        private IDisplay display;
+
+        private IOutput output;
+
+
+        private IPowerTube powerTube;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+
+
+            
+            powerTube = Substitute.For<IPowerTube>();
+            display = Substitute.For<IDisplay>();
+            output = Substitute.For<IOutput>();
+
+
+            timer = new Timer();
+            light = new Light(output);
+            cooker = new CookController(timer, display, powerTube);
+
+
+
+            doorSut = new Door();
+            powerButtonSut = new Button();
+            timeButtonSut = new Button();
+            startCancelButtonSut = new Button();
+
+            UI = new UserInterface(powerButtonSut, timeButtonSut, startCancelButtonSut, doorSut, display, light, cooker);
+
+
+            cooker.UI = UI;
+
+            //State is ready by default
+
+        }
+
+        [Test]
+        public void StateSetTime_StartCancelPressed_TimerTicked()
+        {
+            //Arrange
+            powerButtonSut.Press(); // State set to SetPower
+            timeButtonSut.Press(); //State set to SetTime
+            timeButtonSut.Press(); //State set to SetTime with 2 seconds
+
+            //Act
+            startCancelButtonSut.Press();
+            display.ClearReceivedCalls();
+
+            Thread.Sleep(1000);
+
+            //Assert
+            display.Received(1).ShowTime(Arg.Any<int>(), Arg.Any<int>());
+
+        }
+    }
+}
